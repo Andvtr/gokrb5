e4171fc4c4d2ec0f06b07a939099f8f100f68b4c
a.nikiforov <a.nikiforov@bastion-tech.ru>
2024-10-02 15:57:05 +0300

    added authPak + PKAuthenticator

--- v8/types/Authenticator.go
+++ v8/types/Authenticator.go
@@ -3,6 +3,7 @@ package types
 
 import (
 	"crypto/rand"
+	goasn1 "encoding/asn1"
 	"fmt"
 	"math"
 	"math/big"
@@ -12,6 +13,7 @@ import (
 	"github.com/cobraqxx/gokrb5/v8/iana"
 	"github.com/cobraqxx/gokrb5/v8/iana/asnAppTag"
 	"github.com/jcmturner/gofork/encoding/asn1"
+	"golang.org/x/sys/windows"
 )
 
 // Authenticator - A record containing information that can be shown to have been recently generated using the session
@@ -78,4 +80,115 @@ func (a *Authenticator) Marshal() ([]byte, error) {
 	}
 	b = asn1tools.AddASNAppTag(b, asnAppTag.Authenticator)
 	return b, nil
+}
+
+//	PKAuthenticator ::= SEQUENCE {
+//		cusec [0] INTEGER (0..999999),
+//		ctime [1] KerberosTime,
+//		-- cusec and ctime are used as in [RFC4120], for
+//		-- replay prevention.
+//		nonce [2] INTEGER (0..4294967295),
+//		-- Chosen randomly; this nonce does not need to
+//		-- match with the nonce in the KDC-REQ-BODY.
+//		paChecksum [3] OCTET STRING OPTIONAL,
+//		-- MUST be present.
+//		-- Contains the SHA1 checksum, performed over
+//		-- KDC-REQ-BODY.
+//		...
+//		}
+//
+// https://www.rfc-editor.org/rfc/pdfrfc/rfc4556.txt.pdf
+type PKAuthenticator struct {
+	Cusec int       `asn1:"explicit,tag:0"`
+	CTime time.Time `asn1:"generalized,explicit,tag:1"`
+	nonce uint32    `asn1:"explicit,tag:2"`
+	Cksum []byte    `asn1:"explicit,optional,tag:3"`
+}
+
+// NewAuthenticator creates a new PKAuthenticator.
+func NewPKAuthenticator() (PKAuthenticator, error) {
+	seq, err := rand.Int(rand.Reader, big.NewInt(math.MaxUint32))
+	if err != nil {
+		return PKAuthenticator{}, err
+	}
+	t := time.Now().UTC()
+	return PKAuthenticator{
+		Cusec: int((t.UnixNano() / int64(time.Microsecond)) - (t.Unix() * 1e6)),
+		CTime: t,
+		nonce: uint32(seq.Int64()),
+		Cksum: []byte{0},
+	}, nil
+}
+
+//	AuthPack ::= SEQUENCE {
+//		pkAuthenticator [0] PKAuthenticator,
+//		clientPublicValue [1] SubjectPublicKeyInfo OPTIONAL,
+//		-- Type SubjectPublicKeyInfo is defined in
+//		-- [RFC3280].
+//		-- Specifies Diffie-Hellman domain parameters
+//		-- and the client’s public key value [IEEE1363].
+//	   Zhu & Tung Standards Track [Page 10]
+//	   RFC 4556 PKINIT June 2006
+//		-- The DH public key value is encoded as a BIT
+//		-- STRING according to [RFC3279].
+//		-- This field is present only if the client wishes
+//		-- to use the Diffie-Hellman key agreement method.
+//		supportedCMSTypes [2] SEQUENCE OF AlgorithmIdentifier
+//		OPTIONAL,
+//		-- Type AlgorithmIdentifier is defined in
+//		-- [RFC3280].
+//		-- List of CMS algorithm [RFC3370] identifiers
+//		-- that identify key transport algorithms, or
+//		-- content encryption algorithms, or signature
+//		-- algorithms supported by the client in order of
+//		-- (decreasing) preference.
+//		clientDHNonce [3] DHNonce OPTIONAL,
+//		-- Present only if the client indicates that it
+//		-- wishes to reuse DH keys or to allow the KDC to
+//		-- do so (see Section 3.2.3.1).
+//		...
+//		}
+//
+// https://www.rfc-editor.org/rfc/pdfrfc/rfc4556.txt.pdf
+type AuthPack struct {
+	authentificator PKAuthenticator `asn1:"explicit,tag:0"`
+	// TODO
+	cert windows.CertPublicKeyInfo `asn1:"optional,explicit,tag:1"`
+	// TODO
+	//supportedCMSTypes []windows.CryptAlgorithmIdentifier `asn1:"optional,explicit,tag:2"`
+	clientDHNonce []byte `asn1:"optional,string,tag:3"`
+}
+
+// NewAuthenticator creates a new PKAuthenticator.
+func NewAuthPack(pk PKAuthenticator) (AuthPack, error) {
+	return AuthPack{
+		authentificator: pk,
+		clientDHNonce:   []byte{0},
+		// TODO
+	}, nil
+}
+
+// Marshal the AuthPack.
+func (a *AuthPack) Marshal() ([]byte, error) {
+	pk, err := goasn1.MarshalWithParams(a.authentificator, "context,tag:0")
+	if err != nil {
+		return nil, err
+	}
+
+	c, err := goasn1.MarshalWithParams(a.cert, "context,tag:1")
+	if err != nil {
+		return nil, err
+	}
+
+	dh, err := goasn1.MarshalWithParams(a.clientDHNonce, "context,tag:3")
+	if err != nil {
+		return nil, err
+	}
+
+	ap := append(pk, c...)
+	ap = append(ap, dh...)
+
+	ap = asn1tools.AddASNAppTag(ap, asn1.TagSequence)
+
+	return ap, nil
 }
